f4efa413b87baed93cee99ce7d89766d115e9701
YOLO-GIT <ahmadsufi345@gmail.com>
2024-01-28 23:20:36 +0800

    Some Changes

--- app/components/AboutUs.js
+++ app/components/AboutUs.js
@@ -1,12 +1,80 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Linking,
+  Image,
+} from "react-native";
+import React from "react";
+
+const githubLogo = require("../assets/github_536452.png");
+const twitterLogo = require("../assets/twitter_5968830.png");
+const tiktokLogo = require("../assets/tik-tok_3046126.png");
 
 const AboutUs = () => {
-  return (
-    <View>
-      <Text>AboutUs</Text>
-    </View>
-  )
-}
+  // Function to open social media profiles in browser
+  const openSocialMedia = (url) => {
+    Linking.openURL(url);
+  };
 
-export default AboutUs
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Noty!</Text>
+      <Text style={styles.description}>
+        Noty is a simple yet powerful note-taking app designed to help you stay
+        organized and productive. With Noty, you can easily create, edit, and
+        organize your notes, whether it's for work, school, or personal use.
+      </Text>
+      <Text style={styles.description}>Connect with us on social media:</Text>
+      <View style={styles.socialLinks}>
+        <TouchableOpacity
+          onPress={() => openSocialMedia("https://github.com/YOLO-GIT")}
+        >
+          <Image source={githubLogo} style={styles.socialLogo} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => openSocialMedia("https://twitter.com/YOLO_LV9999999")}
+        >
+          <Image source={twitterLogo} style={styles.socialLogo} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() =>
+            openSocialMedia("https://www.tiktok.com/@glacierzpole")
+          }
+        >
+          <Image source={tiktokLogo} style={styles.socialLogo} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  socialLinks: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  socialLogo: {
+    width: 40,
+    height: 40,
+  },
+});
+
+export default AboutUs;
